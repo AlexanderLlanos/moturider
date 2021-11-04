ccf50e196edaaf9b198c61dc06eab48ad1a78eb0
alexanderllanos <alexander.asdfg@gmail.com>
2021-11-04 12:16:54 -0500

    feat(index): landing

---
+++ js/main.js
@@ -0,0 +1,53 @@
+$(document).click(function () {
+  $(".dropdown-list").fadeOut();
+  $(".g-input--material input").each(function () {
+    if ($(this).val() === "") {
+      $(this).removeClass("focus");
+      $(this).siblings(".main-input").removeClass("focus");
+      $(this).next(".placeholder").removeClass("focus");
+    }
+  });
+});
+
+/*MATERIAL INPUT*/
+$(".g-input--material").on("click", function (e) {
+  $(this).find(".placeholder, input, .main-input").addClass("focus");
+  e.stopPropagation();
+});
+
+var number = 0;
+$("#minus").click(function(){
+  number -= 1;
+  if (number <= 0) number = 0;
+  setNumber(number);
+  $("#number").addClass('bounce');
+  removeAnimation();
+});
+
+$("#plus").click(function(){
+  console.log(number)
+  number += 1;
+  setNumber(number);
+  $("#number").addClass('bounce')
+  removeAnimation();
+});
+
+function removeAnimation() {
+  setTimeout(function(){
+    $("#number").removeClass('bounce'); 
+  }, 100);
+}
+
+function setNumber(number) {
+  // $("#number").attr('data-content', number);
+  $("#number").text(number);
+}
+
+$('nav-link').click(function(e){				
+  e.preventDefault();		//evitar el eventos del enlace normal
+  var strAncla=$(this).attr('href'); //id del ancla
+    $('body,html').stop(true,true).animate({				
+      scrollTop: $(strAncla).offset().top
+    },1000);
+  
+});
